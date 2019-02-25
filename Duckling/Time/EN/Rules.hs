1769fd9152f9e882aaa2648c94ed2e749d9a0d3f
Cristi Cobzarenco <cristi.cobzarenco@gmail.com>
2019-02-25 18:34:13 +0000

    (reinfer) time: add <dd> month <yy>

--- Duckling/Time/EN/Rules.hs
+++ Duckling/Time/EN/Rules.hs
@@ -456,11 +456,11 @@ ruleYearMonthDOM = Rule
       _ -> Nothing
   }
 
-ruleDOMOrdinalMonthYear :: Rule
-ruleDOMOrdinalMonthYear = Rule
+ruleDOMMonthYear :: Rule
+ruleDOMMonthYear = Rule
   { name = "<day-of-month> (ordinal) <named-month> year"
   , pattern =
-    [ Predicate isDOMOrdinal
+    [ Predicate isDOMValue
     , Predicate isAMonth
     , regex "(\\d{2,4})"
     ]
@@ -2234,7 +2234,7 @@ rules =
   , ruleMonthDOMNumeral
   , ruleDOMMonth
   , ruleDOMOfMonth
-  , ruleDOMOrdinalMonthYear
+  , ruleDOMMonthYear
   , ruleYearMonthDOM
   , ruleIdesOfMonth
   , ruleTODLatent
